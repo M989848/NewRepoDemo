ae09025bc8d668cebf5e6a0bd8a0263fdd9076bf
LENOVO <LENOVO@shahidh>
2025-03-17 12:46:48 +0530

    MODIFIED CLASS

--- ProjectForLearning/src/test/java/pac1/CreateOrganizationTest.java
+++ ProjectForLearning/src/test/java/pac1/CreateOrganizationTest.java
@@ -5,12 +5,22 @@ import org.testng.annotations.Test;
 public class CreateOrganizationTest {
 @Test
 public void createOrgTest() {
+	
+	String BROWSER=System.getProperty("browser");
+	String URL=System.getProperty("url");
+	String  USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
 	System.out.println(" enter the url");
 	System.out.println(" enter the user name");
 	System.out.println(" enter the password");
 	System.out.println("  click on login button");
 	System.out.println(" click on org link");
 	System.out.println(" create organization");
+	System.out.println(BROWSER);
+	System.out.println(URL);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	
 	
 }
 @Test
