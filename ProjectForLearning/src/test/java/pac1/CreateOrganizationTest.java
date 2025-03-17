8e9685f306d871fe1d7bbd3f7f5478dd36dde8ed
LENOVO <LENOVO@shahidh>
2025-03-17 15:05:54 +0530

    Modified clsses

--- ProjectForLearning/src/test/java/pac1/CreateOrganizationTest.java
+++ ProjectForLearning/src/test/java/pac1/CreateOrganizationTest.java
@@ -10,29 +10,20 @@ public void createOrgTest() {
 	String URL=System.getProperty("url");
 	String  USERNAME=System.getProperty("username");
 	String PASSWORD=System.getProperty("password");
-	System.out.println(" enter the url");
-	System.out.println(" enter the user name");
-	System.out.println(" enter the password");
-	System.out.println("  click on login button");
-	System.out.println(" click on org link");
-	System.out.println(" create organization");
 	System.out.println(BROWSER);
 	System.out.println(URL);
 	System.out.println(USERNAME);
 	System.out.println(PASSWORD);
+	System.out.println("create organization");
+	System.out.println("=====================");
 	
 	
 	
 }
 @Test
 public void modifyOrgTest() {
-	System.out.println(" enter the url");
-	System.out.println(" enter the user name");
-	System.out.println(" enter the password");
-	System.out.println("  click on login button");
-	System.out.println(" click on org link");
-	System.out.println(" enter the industry name");
-	System.out.println(" create organization");
+	System.out.println("create organization with industry");
+	System.out.println("=====================");
 	
 }
 }
--- ProjectForLearning/src/test/java/pac2/AddProductTest.java
+++ ProjectForLearning/src/test/java/pac2/AddProductTest.java
@@ -5,23 +5,13 @@ import org.testng.annotations.Test;
 public class AddProductTest {
 @Test
 public void AddProductToOrgTest() {
-	System.out.println(" enter the url");
-	System.out.println(" enter the user name");
-	System.out.println(" enter the password");
-	System.out.println("  click on login button");
-	System.out.println(" click on product link");
-	System.out.println("  Add the product to the organization");
-	
+	System.out.println("add prouct to organization");
+	System.out.println("=====================");
 }
 @
 Test
 public void ModifyTheProductTest() {
-System.out.println(" enter the url");
-System.out.println(" enter the user name");
-System.out.println(" enter the password");
-System.out.println("  click on login button");
-System.out.println(" click on product link");
-System.out.println("  Add the product to the organization");
-System.out.println("Add one more product");
+	System.out.println(" modify the product");
+	System.out.println("=====================");
 }
 }
