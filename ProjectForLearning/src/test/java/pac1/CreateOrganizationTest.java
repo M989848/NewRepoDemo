638b5d7081f7cfcef50568b2d9bfe609d5c94b91
LENOVO <LENOVO@shahidh>
2025-03-17 11:56:32 +0530

    Added a new project

---
+++ ProjectForLearning/src/test/java/pac1/CreateOrganizationTest.java
@@ -0,0 +1,27 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationTest {
+@Test
+public void createOrgTest() {
+	System.out.println(" enter the url");
+	System.out.println(" enter the user name");
+	System.out.println(" enter the password");
+	System.out.println("  click on login button");
+	System.out.println(" click on org link");
+	System.out.println(" create organization");
+	
+}
+@Test
+public void modifyOrgTest() {
+	System.out.println(" enter the url");
+	System.out.println(" enter the user name");
+	System.out.println(" enter the password");
+	System.out.println("  click on login button");
+	System.out.println(" click on org link");
+	System.out.println(" enter the industry name");
+	System.out.println(" create organization");
+	
+}
+}
---
+++ ProjectForLearning/src/test/java/pac2/AddProductTest.java
@@ -0,0 +1,27 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class AddProductTest {
+@Test
+public void AddProductToOrgTest() {
+	System.out.println(" enter the url");
+	System.out.println(" enter the user name");
+	System.out.println(" enter the password");
+	System.out.println("  click on login button");
+	System.out.println(" click on product link");
+	System.out.println("  Add the product to the organization");
+	
+}
+@
+Test
+public void ModifyTheProductTest() {
+System.out.println(" enter the url");
+System.out.println(" enter the user name");
+System.out.println(" enter the password");
+System.out.println("  click on login button");
+System.out.println(" click on product link");
+System.out.println("  Add the product to the organization");
+System.out.println("Add one more product");
+}
+}
